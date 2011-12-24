abddac6c6c01def05be6d7dff4a6803419a1b674
Brian Cosgrove <cosgroveb@gmail.com>
2011-12-24 09:29:50 -0600

    cleans things up a bit

--- src/newbot_server.erl
+++ src/newbot_server.erl
@@ -4,101 +4,97 @@
 -include_lib("exmpp/include/exmpp.hrl").
 -include_lib("exmpp/include/exmpp_client.hrl").
 
-% -define(USER_NAME,     element(2,newbot_app:get_env(user))).
-% -define(USER_DOMAIN,   element(2,newbot_app:get_env(user_domain))).
-% -define(USER_PASSWORD, element(2,newbot_app:get_env(user_password))).
-% -define(SERVER_DOMAIN, element(2,newbot_app:get_env(server_domain))).
-% -define(SERVER_PORT,   element(2,newbot_app:get_env(server_port))).
-% -define(MUC_ROOM,      element(2,newbot_app:get_env(muc_room))).
--define(USER_NAME,     "jane").
--define(USER_DOMAIN,   "localhost").
 -define(USER_PASSWORD, "password").
 -define(SERVER_DOMAIN, "conference.localhost").
 -define(SERVER_PORT,   5222).
 -define(MUC_ROOM,      "test@conference.localhost/jane").
 
--export([start_link/0, join_chat/0, go/0]).
+-export([start_link/0, join_chat/0, setup_and_join/0]).
 
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2,
          terminate/2, code_change/3]).
 -define(SERVER, ?MODULE).
 -define(DEFAULT_PORT, 1055).
--record(state, {port, lsock, request_count = 0}).
 
 start_link() ->
-      gen_server:start_link({local, ?MODULE}, ?MODULE, [], []).
+  gen_server:start_link({local, ?MODULE}, ?MODULE, [], []).
 
 init([]) ->
-      {ok, []}.
+  application:start(exmpp),
+  {ok, []}.
 
 join_chat() ->
   gen_server:call(?MODULE, join_chat).
 
 %% callbacks
+
 handle_call(join_chat, _From, _State) ->
-    Pid = spawn(fun newbot_server:go/0),
-    {reply, joining, Pid};
+  Pid = spawn(fun newbot_server:setup_and_join/0),
+  {reply, joining, Pid};
+
 handle_call(_Request, _From, State) ->
-    Reply = ok,
-    {reply, Reply, State}.
+  Reply = ok,
+  {reply, Reply, State}.
 
 handle_cast(_Msg, State) ->
-    {noreply, State}.
+  {noreply, State}.
 
 handle_info(_Info, State) ->
-    {noreply, State}.
+  {noreply, State}.
 
 terminate(_Reason, _State) ->
-    ok.
+  ok.
 
 code_change(_OldVsn, State, _Extra) ->
-    {ok, State}.
+  {ok, State}.
 
-go() ->
-  UserName = "jane",
-  UserDomain = "localhost",
+setup_and_join() ->
+  UserJID = "jane@localhost",
   UserPassword = "password",
   ServerDomain = "localhost",
   ServerPort = 5222,
   MucRoom = "test@conference.localhost/jane",
-  application:start(exmpp),
-  application:start(newbot_app),
-  Session = exmpp_session:start({1,0}),
-  JID = exmpp_jid:make(UserName, UserDomain, random),
-  Status = exmpp_presence:set_status( exmpp_presence:available(), "online"),
-  MucNS = "http://jabber.org/protocol/muc",
-  MucTo = <<"to">>,
-  MucFrom = <<"from">>,
-  JoinRoom = exmpp_xml:set_attribute(exmpp_xml:set_attribute(exmpp_xml:append_child(Status,exmpp_xml:element(MucNS,x)),MucTo,MucRoom),MucFrom,"jane@localhost"),
-  JoinRoom2 = exmpp_xml:remove_element(JoinRoom,status),
+
+  [UserName, UserDomain] = string:tokens(UserJID,"@"),
+
+  Session  = exmpp_session:start({1,0}),
+  JID      = exmpp_jid:make(UserName, UserDomain, random),
+  Status   = exmpp_presence:set_status(exmpp_presence:available(), "online"),
+  JoinRoom = join_room_stanza(UserJID, MucRoom, Status),
+
   exmpp_session:auth_info(Session, JID, UserPassword),
-  erlang:display("got here"),
-  erlang:display(["username: " ++ UserName ++ "user domain: " ++ UserDomain ++ "pass: " ++ UserPassword ++ "server domain: " ++ ServerDomain ++ "server port: " ++ ServerPort]),
-  {ok, _StreamId, _Features} = exmpp_session:connect_TCP(Session, ServerDomain, ServerPort),
-  erlang:display("connected"),
+  exmpp_session:connect_TCP(Session, ServerDomain, ServerPort),
   exmpp_session:login(Session, "PLAIN"),
   exmpp_session:send_packet(Session, Status),
-  exmpp_session:send_packet(Session, JoinRoom2),
-  erlang:display("joined"),
+  exmpp_session:send_packet(Session, JoinRoom),
   loop(Session).
+
+join_room_stanza(UserJID, MucRoom, Status) ->
+  exmpp_xml:remove_element(
+    exmpp_xml:set_attribute(
+      exmpp_xml:set_attribute(
+        exmpp_xml:append_child(
+          Status,
+          exmpp_xml:element(
+            "http://jabber.org/protocol/muc",
+            x)
+        ),<<"to">>,MucRoom),
+      <<"from">>,UserJID),
+    status).
 
 loop(MySession) ->
   receive
     stop ->
       exmpp_session:stop(MySession);
     _Record = #received_packet{packet_type=message, raw_packet=Packet, type_attr=Type}  when Type =/= "error" ->
-      % io:format("Received Message stanza:~n~p~n~n", [_Record]),
       handle_packet(MySession, Packet, Type),
       loop(MySession);
     Record when Record#received_packet.packet_type == 'presence' ->
-      io:format("Received Presence stanza:~n~p~n~n", [Record]),
       handle_presence(MySession, Record, Record#received_packet.raw_packet),
       loop(MySession);
     _Record when _Record#received_packet.packet_type == 'iq' ->
-      io:format("Received IQ stanza:~n~p~n~n", [_Record]),
       loop(MySession);
     _Record ->
-      io:format("~n~nReceived other stanza:~n~p~n~n", [_Record]),
       loop(MySession)
     end.
 
