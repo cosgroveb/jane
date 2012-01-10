1cd309c4fbbf08e4e039418e5d56d65b270e1efd
brian cosgrove <cosgroveb@gmail.com>
2012-01-09 22:59:00 -0600

    re-adds "interpolation" logic to allow jane to include information about the message sender when executing commands

--- src/jane_command_server.erl
+++ src/jane_command_server.erl
@@ -1,8 +1,6 @@
 -module(jane_command_server).
 -behavior(gen_server).
 
--include_lib("exmpp/include/exmpp.hrl").
--include_lib("exmpp/include/exmpp_client.hrl").
 -include_lib("jane.hrl").
 
 -export([start_link/0]).
@@ -16,7 +14,6 @@ start_link() ->
   gen_server:start_link({local, ?MODULE}, ?MODULE, [], []).
 
 init([]) ->
-  % application:start(exmpp),
   {ok, nothing, 0}.
 
 %% api callbacks
@@ -25,12 +22,20 @@ handle_call(_Request, _From, State) ->
   {noreply, State}.
 
 handle_cast({To, From, Body, Session}, State) ->
-  case has_valid_command(?COMMANDS, binary_to_list(Body)) of
+  MetaData = [{sender, lists:nth(2,string:tokens(binary_to_list(To), "/"))}],
+  Reply = case has_valid_command(?COMMANDS, binary_to_list(Body)) of
     false ->
-      xmpp_account:send_message(Session, From, To, <<"Sorry, I don't know what you mean.">>);
+      "Sorry, I don't know what you mean.";
+    {"", ReturnText} ->
+      [interpolate(X, MetaData) ++ " " || X <- string:tokens(ReturnText, " ")];
+    {Command, ReturnText} ->
+      NewCommand = [interpolate(X, MetaData) ++ " " || X <- string:tokens(Command, " ")],
+      _ = os:cmd(NewCommand),
+      [interpolate(X, MetaData) ++ " " || X <- string:tokens(ReturnText, " ")];
     Command ->
-      xmpp_account:send_message(Session, From, To, os:cmd(Command))
+      [interpolate(X, MetaData) ++ " " || X <- string:tokens(Command, " ")]
   end,
+  xmpp_account:send_message(Session, From, To, Reply),
   {noreply, State}.
 
 handle_info(_Record, State) ->
@@ -54,3 +59,18 @@ command({_Word,Command}) ->
   Command;
 command({_Word,Command,ReturnText}) ->
   {Command,ReturnText}.
+
+interpolate([Sigil|Name],MetaData) when [Sigil] =:= "$" ->
+  case get_metadata(list_to_atom(Name),MetaData) of
+    false -> [Sigil] ++ Name;
+    Value -> re:replace(Value,"[\\W]", "_", [global, {return, list}])
+  end;
+interpolate(Term,_) ->
+  Term.
+
+get_metadata(_Key,[{_Key, Value}|_]) ->
+  Value;
+get_metadata(_,[_|[]]) ->
+  false;
+get_metadata(Key,[_|Tail]) ->
+  get_metadata(Key,Tail).
