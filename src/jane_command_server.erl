06c870b8967d5a94a3ece2ab61e8d4f4a2c56e8d
brian cosgrove <cosgroveb@gmail.com>
2012-01-09 21:33:29 -0600

    completes moving command handling logic into jane_command_server

--- src/jane_command_server.erl
+++ src/jane_command_server.erl
@@ -24,8 +24,13 @@ init([]) ->
 handle_call(_Request, _From, State) ->
   {noreply, State}.
 
-handle_cast({Session, RawPacket, TypeAttr}, State) ->
-  handle_message(Session, RawPacket, TypeAttr),
+handle_cast({To, From, Body, Session}, State) ->
+  case has_valid_command(?COMMANDS, binary_to_list(Body)) of
+    false ->
+      xmpp_account:send_message(Session, From, To, <<"Sorry, I don't know what you mean.">>);
+    Command ->
+      xmpp_account:send_message(Session, From, To, os:cmd(Command))
+  end,
   {noreply, State}.
 
 handle_info(_Record, State) ->
@@ -36,60 +41,6 @@ terminate(_Reason, _State) ->
 
 code_change(_OldVsn, State, _Extra) ->
   {ok, State}.
-
-%% Handle messages
-handle_message(_Session, Packet, _TypeAttr="error") ->
-  io:format("~nError: ~p~n",[Packet]);
-handle_message(Session, Packet, TypeAttr) ->
-  Body = exmpp_message:get_body(Packet),
-  To   = exmpp_xml:get_attribute(Packet, <<"from">>, "unknown"),
-  From = exmpp_xml:get_attribute(Packet, <<"to">>, "unknown"),
-  [MucID|Resource] = string:tokens(binary_to_list(To), "/"),
-  SendPkt = exmpp_xml:set_attribute(
-    exmpp_xml:set_attribute(
-      exmpp_xml:set_attribute(
-        exmpp_xml:append_child(
-          exmpp_xml:element("jabber:client",
-            message),
-          exmpp_xml:append_cdata(exmpp_xml:element("jabber:client", body), create_response(Body,[{sender,Resource}]))),
-        <<"from">>,
-        From),
-      <<"to">>,
-      MucID),
-    <<"type">>,
-    TypeAttr),
-  exmpp_session:send_packet(Session, SendPkt).
-
-%% Handle bot commands here and return message body for XMPP reply
-create_response(Body,MetaData) when is_binary(Body) and is_list(MetaData) ->
-  case has_valid_command(?COMMANDS,binary_to_list(Body)) of
-    false ->
-      string:concat("I don't understand ", Body);
-    {"", ReturnText} ->
-      _NewReturnText = [interpolate(X, MetaData) ++ " " || X <- string:tokens(ReturnText, " ")];
-    {Command, ReturnText} ->
-      NewCommand = [interpolate(X, MetaData) ++ " " || X <- string:tokens(Command, " ")],
-      os:cmd(NewCommand),
-      _NewReturnText = [interpolate(X, MetaData) ++ " " || X <- string:tokens(ReturnText, " ")];
-    Command ->
-      NewCommand = [interpolate(X, MetaData) ++ " " || X <- string:tokens(Command, " ")],
-      os:cmd(NewCommand)
-  end.
-
-interpolate([Sigil|Name],MetaData) when [Sigil] =:= "$" ->
-  case get_metadata(list_to_atom(Name),MetaData) of
-    false -> [Sigil] ++ Name;
-    Value -> re:replace(Value,"[\\W]", "_", [global, {return, list}])
-  end;
-interpolate(Term,_) ->
-  Term.
-
-get_metadata(_Key,[{_Key, Value}|_]) ->
-  Value;
-get_metadata(_,[_|[]]) ->
-  false;
-get_metadata(Key,[_|Tail]) ->
-  get_metadata(Key,Tail).
 
 has_valid_command([], _) ->
   false;
--- src/jane_server.erl
+++ src/jane_server.erl
@@ -26,12 +26,7 @@ init([]) ->
 handle_info(Record, #state{session=Session}) when ?IS_GROUP_MESSAGE(Record) ->
   case xmpp_account:get_message(?MUC_ROOM, Record) of
     {To, From, Body} ->
-      case has_valid_command(?COMMANDS, binary_to_list(Body)) of
-        false ->
-          xmpp_account:send_message(Session, From, To, <<"Sorry, I don't know what you mean.">>);
-        Command ->
-          xmpp_account:send_message(Session, From, To, os:cmd(Command))
-      end;
+      gen_server:cast(jane_command_server, {To, From, Body, Session});
       _ -> pass
   end,
   {noreply, #state{session=Session}};
@@ -53,16 +48,3 @@ terminate(_Reason, _State) ->
 code_change(_OldVsn, State, _Extra) ->
   {ok, State}.
 
-% Private Functions
-has_valid_command([], _) ->
-  false;
-has_valid_command([Vocabulary|Tail],Body) ->
-  case string:str(Body,element(1, Vocabulary)) of
-    0 -> has_valid_command(Tail, Body);
-    _ -> command(Vocabulary)
-  end.
-
-command({_Word,Command}) ->
-  Command;
-command({_Word,Command,ReturnText}) ->
-  {Command,ReturnText}.
