4f77b57fa305b9c2388d607cc4bad7c225416f1f
Brian Cosgrove <cosgroveb@gmail.com>
2012-01-15 16:45:30 -0600

    makes run_dev_console script automatically start jane application and load sync:go

--- src/jane_app.erl
+++ src/jane_app.erl
@@ -2,17 +2,27 @@
 
 -behaviour(application).
 
+-export([start/0]).
 %% Application callbacks
 -export([start/2, stop/1]).
 
 %% ===================================================================
+%% API
+%% ===================================================================
+start() ->
+  start("","").
+
+%% ===================================================================
 %% Application callbacks
 %% ===================================================================
-
 start(_StartType, _StartArgs) ->
   application:start(crypto),
   application:start(public_key),
   application:start(ssl),
+  application:start(inets),
+  application:start(mochiweb),
+  application:start(webmachine),
+  application:start(jane_control),
   ibrowse:start(),
 
   error_logger:info_msg("Starting jane_app~n"),
