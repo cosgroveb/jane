0b7b32452ef1472d71ba187e8721ff7e206506ee
Brian Cosgrove <cosgroveb@gmail.com>
2012-02-08 20:11:49 -0600

    jane_irc_server parses the IRC packets we care about

--- test/jane_irc_server_test.erl
+++ test/jane_irc_server_test.erl
@@ -13,3 +13,15 @@ is_end_of_motd_test() ->
 is_ping_test() ->
   SplitInput = string:tokens("PING :brians-macbook-pro.local\r\n", ": "),
   ?assert(jane_irc_server:is_ping(SplitInput) =:= true).
+
+parse_irc_packet_test() ->
+  ChatPacket = ":cosgroveb!~cosgroveb@127.0.y.p PRIVMSG #test :jane hello\r\n",
+  EndOfMOTDPacket = ":brians-macbook-pro.local 376 jane :End of /MOTD command.\r\n",
+  PingPacket = "PING :brians-macbook-pro.local\r\n",
+  ParsedChat = jane_irc_server:parse_irc_packet(ChatPacket),
+  ParsedEndMOTD = jane_irc_server:parse_irc_packet(EndOfMOTDPacket),
+  ParsedPing = jane_irc_server:parse_irc_packet(PingPacket),
+
+  ?assert({irc_chat, "cosgroveb", "#test", "jane"} == ParsedChat),
+  ?assert({irc_end_of_motd, null, null, null} == ParsedEndMOTD),
+  ?assert({irc_ping, null, null, null} == ParsedPing).
