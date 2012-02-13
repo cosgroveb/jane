6acf2073781c86a1f956e33e43eb2fdb5eb50057
Brian Cosgrove <cosgroveb@gmail.com>
2012-02-12 20:52:06 -0600

    completes jane_irc_server gen_server behaviors; updates tests

--- test/jane_irc_server_test.erl
+++ test/jane_irc_server_test.erl
@@ -4,47 +4,81 @@
 
 parse_irc_chat_packet_test() ->
   ChatPacket = ":cosgroveb!~cosgroveb@127.0.y.p PRIVMSG #test :jane hello\r\n",
+  ExpectedPacket = #irc_packet{
+    type=irc_chat,
+    channel="#test",
+    from="cosgroveb",
+    to  ="jane",
+    body=string:concat("jane ",["hello"])
+  },
+
   ParsedChat = jane_irc_server:parse_irc_packet(ChatPacket),
-  ?assert({irc_chat, "cosgroveb", "#test", "jane"} == ParsedChat).
+
+  ?assert(ExpectedPacket == ParsedChat).
 
 parse_irc_endmotd_packet_test() ->
-  EndOfMOTDPacket = ":brians-macbook-pro.local 376 jane :End of /MOTD command.\r\n",
+  EndOfMOTDPacket = ":testhost.local 376 jane :End of /MOTD command.\r\n",
+
   ParsedEndMOTD = jane_irc_server:parse_irc_packet(EndOfMOTDPacket),
-  ?assert({irc_end_of_motd, null, null, null} == ParsedEndMOTD).
+
+  ?assert(#irc_packet{type=irc_end_of_motd, from=null, channel=null, to=null} == ParsedEndMOTD).
 
 parse_irc_ping_test() ->
-  PingPacket = "PING :brians-macbook-pro.local\r\n",
+  PingPacket = "PING :testhost.local\r\n",
+  ExpectedRecord = #irc_packet{
+    type=irc_ping,
+    from=null,
+    channel=null,
+    to=null,
+    body=["testhost.local\r\n"]},
   ParsedPing = jane_irc_server:parse_irc_packet(PingPacket),
-  ?assert({irc_ping, null, null, null} == ParsedPing).
 
-prepare_message_test() ->
+  ?assert(ExpectedRecord  == ParsedPing).
+
+prepare_packet_test() ->
   MessageFromCommandWorker = #message{
     room="#test",
     from=null,
     to  =null,
     body="Hello brian"
   },
-  IrcPacketToSend = jane_irc_server:prepare_message(MessageFromCommandWorker),
   ExpectedOutput  = "PRIVMSG #test :Hello brian\r\n",
 
-  ?assert(IrcPacketToSend == ExpectedOutput).
+  PreparedIrcPacket = jane_irc_server:prepare_packet(MessageFromCommandWorker),
+  Result = ExpectedOutput == PreparedIrcPacket,
+  ?assert(Result).
+
+prepare_join_packet_test() ->
+  Room = "#test",
+  ExpectedPacket = "JOIN :#test\r\n",
+
+  JoinRoomPacket = jane_irc_server:prepare_join_packet(Room),
+
+  ?assert(ExpectedPacket == JoinRoomPacket).
+
+prepare_ping_packet_test() ->
+  Body = "anybody there?",
+  ExpectedPacket = "PONG anybody there?\r\n",
+
+  PingPacket = jane_irc_server:prepare_ping_packet(Body),
+
+  ?assert(ExpectedPacket == PingPacket).
 
 connect_bad_port_test() ->
   Login     = "jane_on_a_bad_port",
-  BadPort   = 6578,
+  BadPort   = "6578",
   Domain    = "127.0.0.1",
-  ?assertExit({error, connection_failed}, jane_irc_server:connect(Login, BadPort, Domain)).
+
+  ?assertExit(connection_failed, jane_irc_server:connect(Login, BadPort, Domain)).
 
 connect_test() ->
   _Pid   = spawn(fun irc_conn_faker/0),
   Login  = "jane",
-  Port   = 5678,
+  Port   = "5678",
   Domain = "127.0.0.1",
   {ok, Socket} = jane_irc_server:connect(Login, Port, Domain),
 
-  ?assert(is_port(Socket)),
-
-  jane_irc_server:disconnect(Socket).
+  ?assert(is_port(Socket)).
 
 irc_conn_faker() ->
   {ok, ListenSocket} = gen_tcp:listen(5678, [{active, false}]),
